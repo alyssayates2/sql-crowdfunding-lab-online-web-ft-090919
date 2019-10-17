71a34a35a1c6491a10d3f8413842a16aed7ecafd
Alyssa Yates <bosslys@icloud.com>
2019-10-17 16:57:31 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -5,13 +5,13 @@ CREATE TABLE projects (
   funding_goal INTEGER,
   start_date INTEGER,
   end_date INTEGER
-  );
-  CREATE TABLE users (
+);
+CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER
-  );
-  CREATE TABLE pledges (
+);
+CREATE TABLE pledges (
   id INTEGER PRIMARY KEY,
   amount INTEGER,
   user_id INTEGER,
