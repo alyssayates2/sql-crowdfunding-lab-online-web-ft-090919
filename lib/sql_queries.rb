ffd45cfc0b4f8b6e8d3d12df41dc555149d48113
Alyssa Yates <bosslys@icloud.com>
2019-10-17 16:54:13 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,7 @@ end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT projects.title, projects.funding_goal, pledges.amount, pledges.project_id
-  FROM projects
+  FROM projects, pledges
   ;"
 end
 
